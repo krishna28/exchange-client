5026611d32819681750a0edb7a460d0c5510136a
krishnakant singh <searchkisu@gmail.com>
2017-04-18 12:12:02 +0530

    minified and build

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'exchange-rate',
+          module: 'exchange',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
