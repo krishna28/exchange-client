fa75051c9dc071ac294c4ab4f51d5b4782d0c8c1
krishnakant singh <searchkisu@gmail.com>
2017-04-18 09:43:27 +0530

    fixed issue with build

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -1,53 +1,52 @@
 (function(){
 	
-'use strict'
+  'use strict'
 
-angular.module('appController', [])
-	 .controller('MainCtrl',function($scope,$q,MainService,Notification,$timeout){
+  angular.module('appController', [])
+  .controller('MainCtrl',function($scope,$q,MainService,Notification,$timeout){
     //
-      var mainVm = this;
-      mainVm.loading = true;  
-      mainVm.unitOfCurr = 1;
-      mainVm.currentDate = new Date();
+    var mainVm = this;
+    mainVm.loading = true;  
+    mainVm.unitOfCurr = 1;
+    mainVm.currentDate = new Date();
 
      //function to assign data after ajax call
-      var assignData = function assignData(result,history=null){
-        console.log("data s",result);
-          if(result[0].status == 200){
-          var exchangeRate = result[0].data;
-          var currencies = result[1].data;
-          var exchangeList = [];
-          for (var x in exchangeRate.rates) {
-                exchangeList.push({
-                    "currencyName": currencies[x],
-                    "code": x,
-                    "rate": exchangeRate.rates[x]
-                });
-          }
-           $timeout(function() {
-            mainVm.exchangeList = exchangeList;
-           }, 0);
-          mainVm.exchangeList = exchangeList;
-          mainVm.currencies = currencies;
-          mainVm.loading = false; 
-          if(history){      
-          mainVm.updating = false;  
-          }  
-        }else{
-          mainVm.loading = false;
-          var errorMsg = result[0].data.description || "500 error,will be fixed soon"
-          Notification.error(errorMsg);
+     var assignData = function assignData(result,history=null){
+      if(result[0].status == 200){
+        var exchangeRate = result[0].data;
+        var currencies = result[1].data;
+        var exchangeList = [];
+        for (var x in exchangeRate.rates) {
+          exchangeList.push({
+            "currencyName": currencies[x],
+            "code": x,
+            "rate": exchangeRate.rates[x]
+          });
         }
-     };
+        $timeout(function() {
+          mainVm.exchangeList = exchangeList;
+        }, 0);
+        mainVm.exchangeList = exchangeList;
+        mainVm.currencies = currencies;
+        mainVm.loading = false; 
+        if(history){      
+          mainVm.updating = false;  
+        }  
+      }else{
+        mainVm.loading = false;
+        var errorMsg = result[0].data.description || "500 error,will be fixed soon"
+        Notification.error(errorMsg);
+      }
+    };
 
-     mainVm.changeSelectedItem = function changeSelectedItem(){
+    mainVm.changeSelectedItem = function changeSelectedItem(){
       mainVm.calculateExchange();
-     }
+    }
 
 
       // promise call to multiple service on application startup
       $q.all([MainService.getLatestExchange(), MainService.getCurrencies()]).then(function (result) {
-          assignData(result);
+        assignData(result);
       });
 
       // service to fetch old exchange rate based on specific date
@@ -66,18 +65,18 @@ angular.module('appController', [])
         var finalDateSting = year.concat("-").concat(month).concat("-").concat(dayn);
         $q.all([MainService.getHistoryExchange(finalDateSting), MainService.getCurrencies()]).then(function (result) {
           assignData(result,true);
-      });      
-      
+        });      
+
       };
 
       //calculate  exchange rate based on source and target currency
       mainVm.calculateExchange =  function calculateExchange(){
         if(mainVm.fromCurrency && mainVm.toCurrency){
-        var baseRateInUsd = (1/(+mainVm.fromCurrency.rate));
-        var finalResult = baseRateInUsd  * (+mainVm.toCurrency.rate) * (+mainVm.unitOfCurr);
-         mainVm.result = (+finalResult.toFixed(7)).toString(); 
-         } 
+          var baseRateInUsd = (1/(+mainVm.fromCurrency.rate));
+          var finalResult = baseRateInUsd  * (+mainVm.toCurrency.rate) * (+mainVm.unitOfCurr);
+          mainVm.result = (+finalResult.toFixed(7)).toString(); 
+        } 
       };
 
-     })
+    })
 })();	
--- app/scripts/services/mainService.js
+++ app/scripts/services/mainService.js
@@ -2,65 +2,65 @@
   'use strict'
 
   angular.module('mainService', [])
-    .factory('MainService', function($http,config) {
+  .factory('MainService', function($http,config) {
 
-          var userServiceObject = {};
+    var userServiceObject = {};
 
            //function to get latest update on currencies rate
-          userServiceObject.getLatestExchange = function getLatestExchange(){
+           userServiceObject.getLatestExchange = function getLatestExchange(){
             return $http.get(config.apiUrl.concat("latest"));
           };
            //function to get latest update on available currencies
-          userServiceObject.getCurrencies = function getCurrencies(){
+           userServiceObject.getCurrencies = function getCurrencies(){
             return $http.get(config.apiUrl.concat("currencies"));
           };
            //function to get latest update on currencies rate based on specfic date
-          userServiceObject.getHistoryExchange = function getHistoryExchange(finalDateSting){
+           userServiceObject.getHistoryExchange = function getHistoryExchange(finalDateSting){
             return $http.get(config.apiUrl.concat("history?date="+finalDateSting));
           };
 
-         return userServiceObject;
+          return userServiceObject;
 
-    })
-    .factory('AuthInterceptor', function($q, $location, config,$window) {
+        })
+  .factory('AuthInterceptor', function($q, $location, config,$window) {
 
-      var interceptorFactory = {};
+    var interceptorFactory = {};
 
- 
-      interceptorFactory.request = function(config_p) {
+    
+    interceptorFactory.request = function(config_p) {
 
-        if(config.apikey) {
-          config_p.headers['apikey'] = config.apikey;
-        }
-        return config_p;
+      if(config.apikey) {
+        config_p.headers['apikey'] = config.apikey;
+      }
+      return config_p;
 
-      };
-  
-      interceptorFactory.responseError = function(response){
-        if(response.status == 403){
-          console.log("unauthorized error");
-          $window.location = '/403.html';
-          $q.reject(response);
-          
-        }
-        if(response.status == 401){
-          $window.location = '/403.html';
-            return $q.reject(response);
-          
-        }
-        
-        if(response.status == -1){
-          
-            return $q.reject(response);
-        }
-        if(response.status == 500){
-          $location.path("/500.html");
-          return $q.reject(response);
-          
-        }
+    };
+    
+    interceptorFactory.responseError = function(response){
+      if(response.status == 403){
+        console.log("unauthorized error");
+        $window.location = '/403.html';
+        $q.reject(response);
         
       }
-  
-  return interceptorFactory;
-});
+      if(response.status == 401){
+        $window.location = '/403.html';
+        return $q.reject(response);
+        
+      }
+      
+      if(response.status == -1){
+        
+        return $q.reject(response);
+      }
+      if(response.status == 500){
+        $location.path("/500.html");
+        return $q.reject(response);
+        
+      }
+      
+    }
+    
+    return interceptorFactory;
+  });
 })();
